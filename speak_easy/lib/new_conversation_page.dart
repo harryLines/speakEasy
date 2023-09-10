62b1eab2bc58e9c7f90f409762c3a1e7adb06a08
psyhl6 <psyhl6@nottingham.ac.uk>
2023-09-10 16:17:13 +0100

    Add ability to search for and start a conversation with any user using their email

--- speak_easy/lib/new_conversation_page.dart
+++ speak_easy/lib/new_conversation_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:speak_easy/conversation.dart';
+import "main.dart";
 
 class NewConversationPage extends StatefulWidget {
   @override
@@ -7,7 +9,7 @@ class NewConversationPage extends StatefulWidget {
 }
 
 class _NewConversationPageState extends State<NewConversationPage> {
-  String _searchText = '';
+  String _searchText = ''; // Define _searchText here
 
   @override
   Widget build(BuildContext context) {
@@ -20,31 +22,26 @@ class _NewConversationPageState extends State<NewConversationPage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: <Widget>[
-            TextField(
-              decoration: InputDecoration(
-                hintText: 'Search for people to chat with',
-              ),
-              onChanged: (text) {
-                setState(() {
-                  _searchText = text;
-                });
-              },
-            ),
-            SizedBox(height: 16.0),
+            // ...
+
             Expanded(
-              child: FutureBuilder<List<String>>(
+              child: FutureBuilder<List<Map<String, dynamic>>>(
                 future: _searchUsers(_searchText),
                 builder: (BuildContext context,
-                    AsyncSnapshot<List<String>> snapshot) {
+                    AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
                   if (snapshot.hasData) {
                     return ListView.builder(
                       itemCount: snapshot.data!.length,
                       itemBuilder: (BuildContext context, int index) {
-                        final email = snapshot.data![index];
+                        final userData = snapshot.data![index];
+                        final email = userData['Email'];
+                        final UserID = userData['UserID'];
+
                         return ListTile(
                           title: Text(email),
                           onTap: () {
-                            // Navigate to chat page with selected user
+                            // Create a new conversation with the selected user
+                            _createNewConversation(UserID);
                           },
                         );
                       },
@@ -61,26 +58,57 @@ class _NewConversationPageState extends State<NewConversationPage> {
     );
   }
 
-  Future<List<String>> _searchUsers(String searchText) async {
+  // Create a new conversation with the selected user
+  void _createNewConversation(String selectedUserId) async {
     try {
-      // Initialize Firestore
       final FirebaseFirestore firestore = FirebaseFirestore.instance;
 
-      // Create a reference to the users collection in Firestore
-      final CollectionReference usersCollection = firestore.collection('Users');
+      // Create a new conversation in the Conversations collection
+      final newConversation = await firestore.collection('Conversations').add({
+        'users': [
+          currentUserID,
+          selectedUserId
+        ], // Add the user IDs to the conversation
+      });
 
-      // Query Firestore to get users whose email contains the search text
-      final QuerySnapshot userSnapshot = await usersCollection
+      final conversationId = newConversation.id;
+
+      // Navigate to the chat page with the new conversation
+      Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ConversationScreen(
+            conversationId: conversationId,
+          ),
+        ),
+      );
+    } catch (e) {
+      // Handle any potential errors here
+      print('Error creating a new conversation: $e');
+    }
+  }
+
+  // ...
+
+  Future<List<Map<String, dynamic>>> _searchUsers(String searchText) async {
+    try {
+      final FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+      final QuerySnapshot userSnapshot = await firestore
+          .collection('Users')
           .where('Email', isGreaterThanOrEqualTo: searchText)
           .where('Email', isLessThan: searchText + 'z')
           .get();
-      // Extract user emails from the snapshot
-      final matchingUsers =
-          userSnapshot.docs.map((doc) => doc['Email'] as String).toList();
+
+      final matchingUsers = userSnapshot.docs.map((doc) {
+        return {
+          'Email': doc['Email'] as String,
+          'UserID': doc['UserID'] as String,
+        };
+      }).toList();
 
       return matchingUsers;
     } catch (e) {
-      // Handle any potential errors here
       print('Error searching users: $e');
       return [];
     }
